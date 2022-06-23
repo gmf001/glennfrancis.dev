d0b3ce066d4d73770641f6f95e35edbec4c9f33a
Glenn Francis <xglennfrancisx@gmail.com>
2022-06-23 22:54:17 +1000

    added robots

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,15 @@
-import { defineConfig } from "astro/config";
+import { defineConfig } from 'astro/config';
+import tailwind from '@astrojs/tailwind';
 
-import tailwind from "@astrojs/tailwind";
+import astro from 'astro-robots-txt';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
-  vite: {
-    ssr: {
-      external: ["svgo"],
-    },
-  },
+	integrations: [tailwind(), astro()],
+	experimental: { integrations: true },
+	vite: {
+		ssr: {
+			external: ['svgo']
+		}
+	}
 });
