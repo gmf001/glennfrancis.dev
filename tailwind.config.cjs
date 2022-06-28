f46b14996c4e91d9ec119d32b53b070f2eed6416
Glenn Francis <xglennfrancisx@gmail.com>
2022-06-28 11:21:36 +1000

    inter font, new images

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,6 +3,9 @@ module.exports = {
   content: ['./src/**/*.{astro,html,js,jsx,md,svelte,ts,tsx,vue}'],
   theme: {
     extend: {
+      fontFamily: {
+        inter: ['Inter', 'sans-serif']
+      },
       colors: {
         gray: {
           50: '#fafafa',
